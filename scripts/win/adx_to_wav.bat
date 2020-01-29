bec0014f4eb17314cf76f7d9489c461333443c46
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2020-01-30 00:46:25 +0500

    Rewrote Batch scripts to directly convert files

--- scripts/win/adx_to_wav.bat
+++ scripts/win/adx_to_wav.bat
@@ -1,12 +1,12 @@
 @echo off
+setlocal EnableDelayedExpansion
 call ffmpeg.exe -version && (
 echo.
 
-where /Q /R SOUND *.adx && robocopy SOUND SOUND_raw *.adx /S > NUL
-
-for /F "delims==" %%f in ('where /F /R SOUND_raw *.adx') DO (
+for /F "delims==" %%f in ('where /F /R SOUND *.adx') DO (
     echo %%f
-    call ffmpeg.exe -y -loglevel fatal -hide_banner -nostats -i %%f "%%~dpnf.wav" && (
-    del %%f )
+    set "output=%%~dpnf.wav"
+    set "output="!output:SOUND=SOUND_raw!"" 
+    call ffmpeg.exe -y -loglevel fatal -hide_banner -nostats -i %%f !output!
 ))
 pause
--- scripts/win/convert_to_adx.bat
+++ scripts/win/convert_to_adx.bat
@@ -1,12 +1,12 @@
 @echo off
+setlocal EnableDelayedExpansion
 call ffmpeg.exe -version && (
 echo.
 
-where /Q /R SOUND_raw *.wav *.mp3 *.flac && robocopy SOUND_raw SOUND *.wav *.mp3 *.flac /S > NUL
-
-for /F "delims==" %%f in ('where /F /R SOUND *.wav *.mp3 *.flac') DO (
+for /F "delims==" %%f in ('where /F /R SOUND_raw *.wav *.mp3 *.flac') DO (
     echo %%f
-    call ffmpeg.exe -y -loglevel fatal -hide_banner -nostats -i %%f "%%~dpnf.adx" && (
-    del %%f)
+    set "output=%%~dpnf.adx"
+    set "output="!output:SOUND_raw=SOUND!"" 
+    call ffmpeg.exe -y -loglevel fatal -hide_banner -nostats -i %%f !output!
 ))
 pause
