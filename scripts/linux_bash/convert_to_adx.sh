ae4a95452add8ebdcdc1ce24c6b2453ddd01afec
OSA413 <OSA413@users.noreply.github.com>
2020-01-30 11:23:57 +0500

    Fix when paret folder doesn't exist (bash)

--- scripts/linux_bash/convert_to_adx.sh
+++ scripts/linux_bash/convert_to_adx.sh
@@ -1,10 +1,12 @@
 convert()
 {
 for file in ./$3/**/*.$1; do
+    [ ! -e $file ] && continue
     echo $file
     output="${file%.*}".$2
     output="${output/$3/$4}"
     
+    mkdir -p $(dirname $output)
     ffmpeg -y -loglevel fatal -hide_banner -nostats -i $file $output
 done
 }
