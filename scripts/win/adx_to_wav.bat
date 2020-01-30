efca81ff0e5cd61867abcf1f96cf00c9221424da
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2020-01-30 10:06:10 +0500

    Fix when paret folder doesn't exist (bat)

--- scripts/win/adx_to_wav.bat
+++ scripts/win/adx_to_wav.bat
@@ -6,7 +6,13 @@ echo.
 for /F "delims==" %%f in ('where /F /R SOUND *.adx') DO (
     echo %%f
     set "output=%%~dpnf.wav"
-    set "output="!output:SOUND=SOUND_raw!"" 
+    set "output="!output:SOUND=SOUND_raw!""
+
+    if not exist !output! (
+    mkdir !output!
+    rmdir !output!
+    )
+
     call ffmpeg.exe -y -loglevel fatal -hide_banner -nostats -i %%f !output!
 ))
 pause
--- scripts/win/convert_to_adx.bat
+++ scripts/win/convert_to_adx.bat
@@ -6,7 +6,13 @@ echo.
 for /F "delims==" %%f in ('where /F /R SOUND_raw *.wav *.mp3 *.flac') DO (
     echo %%f
     set "output=%%~dpnf.adx"
-    set "output="!output:SOUND_raw=SOUND!"" 
+    set "output="!output:SOUND_raw=SOUND!""
+
+    if not exist !output! (
+    mkdir !output!
+    rmdir !output!
+    )
+
     call ffmpeg.exe -y -loglevel fatal -hide_banner -nostats -i %%f !output!
 ))
 pause
