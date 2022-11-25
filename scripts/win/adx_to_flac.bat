3ae6be15327b363dabf56f974398c77380013d4b
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2022-11-25 21:54:26 +0500

    I hope it fixes the excpamation mark error in the path

--- scripts/win/adx_to_flac.bat
+++ scripts/win/adx_to_flac.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal DisableDelayedExpansion
 set ffmpeg="%~dp0ffmpeg.exe"
 call %ffmpeg% -version && (
 echo.
@@ -7,13 +7,15 @@ echo.
 for /F "delims==" %%f in ('where /F /R SOUND *.adx') DO (
     echo %%f
     set "output=%%~dpnf.flac"
+    setlocal EnableDelayedExpansion
     set "output="!output:SOUND=SOUND_raw!""
+    endlocal
 
-    if not exist !output! (
-    mkdir !output!
-    rmdir !output!
+    if not exist %output% (
+    mkdir %output%
+    rmdir %output%
     )
 
-    call %ffmpeg% -y -loglevel fatal -hide_banner -nostats -i %%f !output!
+    call %ffmpeg% -y -loglevel fatal -hide_banner -nostats -i %%f %output%
 ))
 pause
--- scripts/win/convert_to_adx.bat
+++ scripts/win/convert_to_adx.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal DisableDelayedExpansion
 set ffmpeg="%~dp0ffmpeg.exe"
 call %ffmpeg% -version && (
 echo.
@@ -7,13 +7,15 @@ echo.
 for /F "delims==" %%f in ('where /F /R SOUND_raw *.wav *.mp3 *.flac') DO (
     echo %%f
     set "output=%%~dpnf.adx"
+    setlocal EnableDelayedExpansion
     set "output="!output:SOUND_raw=SOUND!""
+    endlocal
 
-    if not exist !output! (
-    mkdir !output!
-    rmdir !output!
+    if not exist %output% (
+    mkdir %output%
+    rmdir %output%
     )
 
-    call %ffmpeg% -y -loglevel fatal -hide_banner -nostats -i %%f !output!
+    call %ffmpeg% -y -loglevel fatal -hide_banner -nostats -i %%f %output%
 ))
 pause
